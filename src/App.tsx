1fb571e42f0a15ef92eb3ef529c8da8c7ec2a833
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-18 15:57:00 +0800

    Add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
