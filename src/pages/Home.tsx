cd7b34ae884ddf1098d56e35cde6213cd7697f77
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-18 19:59:28 +0800

    Add Sidebar component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 function Home() {
   return (
-    <div>
+    <div className='max-h-screen overflow-hidden'>
+      <div style={{ height: "7.5vh" }}>
         <Navbar />
+      </div>
+      <div className='flex lg:mt-[10px] xl:mt-[-4px] 2xl:mt-[-12px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh]' >{/* style={{ height: "92.5vh" }} */}
+        <Sidebar />
+      </div>
     </div>
   )
 }
