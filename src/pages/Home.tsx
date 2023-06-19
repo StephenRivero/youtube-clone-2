b855f9aa9adf0a93ca9c5d3e408894a5a08ac166
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-19 20:14:47 +0800

    Add Custom Screen
    Added a custom screen for min-screen size 1920px width and
    fixed sidebar for min width 1920px

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
       <div style={{ height: "7.5vh" }}>
         <Navbar />
       </div>
-      <div className='flex lg:mt-[10px] xl:mt-[-4px] 2xl:mt-[-12px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh]' >{/* style={{ height: "92.5vh" }} */}
+      <div className='flex lg:mt-[10px] xl:mt-[-4px] 2xl:mt-[-12px] 3xl:mt-[-25px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh] 3xl:h-[94.7vh]' >{/* style={{ height: "92.5vh" }} */}
         <Sidebar />
       </div>
     </div>
