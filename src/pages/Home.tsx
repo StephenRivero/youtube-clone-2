749430dc29a777eed421b4ad21467a104f01a5e2
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-24 21:29:59 +0800

    Setup Redux Toolkit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
 
 function Home() {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+  }, [dispatch]);
+
   return (
     <div className='max-h-screen overflow-hidden'>
       <div style={{ height: "7.5vh" }}>
