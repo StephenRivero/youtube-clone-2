796026eab8f7508f28310d8196f696c95171fc02
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-25 11:50:37 +0800

    Add styles
    Added a border radius to video card thumbnails and if hovered
    the border radius will disappear

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,13 +7,13 @@ function Card({ data }: { data: HomePageVideos }) {
     // const isData = data ? true : false;
 
     return (
-        <div className='w-64 h-60 flex gap-3 flex-col'>
+        <div className='group w-64 h-60 flex gap-3 flex-col'>
             <div className='relative'>
                 <span className='absolute bottom-3 right-3 text-sm bg-gray-900 px-2 py-0.5 z-10'>
                     {data.videoDuration}
                 </span>
                 <Link to={`/watch/${data.videoId}`}>
-                    <img src={data.videoThumbnail} className='h-44 w-72' alt="Thumbnail" />
+                    <img src={data.videoThumbnail} className='h-44 w-72 rounded-xl group-hover:rounded-none transition' alt="Thumbnail" />
                 </Link>
             </div>
             <div className="flex gap-2">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,7 +28,8 @@ function Home() {
       <div style={{ height: "7.5vh" }}>
         <Navbar />
       </div>
-      <div className='flex lg:mt-[10px] xl:mt-[-4px] 2xl:mt-[-12px] 3xl:mt-[-25px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh] 3xl:h-[94.7vh]' >{/* style={{ height: "92.5vh" }} */}
+      {/* xl:mt-[-4px] */}
+      <div className='flex lg:mt-[10px] 2xl:mt-[-12px] 3xl:mt-[-25px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh] 3xl:h-[94.7vh]' > {/* xl:mt-[-4px] style={{ height: "92.5vh" }} */}
         <Sidebar />
         {videos.length ? (
           <InfiniteScroll
