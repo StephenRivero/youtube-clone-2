f8ab769a7b0e38af81f08d4852c9a63b24df878b
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-25 10:33:29 +0800

    Added thumbnail of video cards

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { HomePageVideos } from '../Types'
+import { Link } from 'react-router-dom'
+
+function Card({ data }: { data: HomePageVideos }) {
+
+    // const isData = data ? true : false;
+
+    return (
+        <div className='w-64 h-60 flex gap-3 flex-col'>
+            <div className='relative'>
+                <span className='absolute bottom-3 right-3 text-sm bg-gray-900 px-2 py-0.5 z-10'>
+                    {data.videoDuration}
+                </span>
+                <Link to={`/watch/${data.videoId}`}>
+                    <img src={data.videoThumbnail} className='h-44 w-72' alt="Thumbnail" />
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,10 @@ import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import { useAppDispatch, useAppSelector } from '../store/hooks'
 import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
+import Spinner from '../components/Spinner';
+import { HomePageVideos } from '../Types';
+import Card from '../components/Card';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Home() {
   const dispatch = useAppDispatch();
@@ -19,6 +23,21 @@ function Home() {
       </div>
       <div className='flex lg:mt-[10px] xl:mt-[-4px] 2xl:mt-[-12px] 3xl:mt-[-25px] h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh] 3xl:h-[94.7vh]' >{/* style={{ height: "92.5vh" }} */}
         <Sidebar />
+        {videos.length ? (
+          <InfiniteScroll
+            dataLength={videos.length}
+            next={() => dispatch(getHomePageVideos(true))}
+            hasMore={videos.length < 500}
+            loader={<Spinner />}
+            height={650}
+          >
+            <div className='grid gap-y-14 gap-x-8 grid-cols-4 p-8'>
+              {videos.map((item: HomePageVideos) => {
+                return <Card data={item} key={item.videoId} />;
+              })}
+            </div>
+          </InfiniteScroll>
+        ):  <Spinner />}
       </div>
     </div>
   )
