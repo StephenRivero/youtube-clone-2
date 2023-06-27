a687d52b43c5d988d1355a6f0ddf7490caa4588d
Stephen Rivero <stephen.m.rivero@gmail.com>
2023-06-27 20:18:36 +0800

    Add final height for videos home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,23 +33,23 @@ function Home() {
       {/*  h-[92.5vh] xl:h-[92.9vh] 2xl:h-[93.8vh] 3xl:h-[94.7vh] */}
       <div className='flex h-[calc(100vh-56px)] w-full'> {/* xl:mt-[-4px] style={{ height: "92.5vh" }} */}
         <Sidebar />
-        {videos.length ? (
-          
-          <InfiniteScroll
-            dataLength={videos.length}
-            next={() => dispatch(getHomePageVideos(true))}
-            hasMore={videos.length < 500}
-            loader={<Spinner />}
-            // height={650}
-            height={1080}
-          >
-            <div className='grid gap-y-14 gap-x-8 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-5 p-4 w-full'> {/* sm:grid-cols-1 */}
-              {videos.map((item: HomePageVideos) => {
-                return <Card data={item} key={item.videoId} />;
-              })}
-            </div>
-          </InfiniteScroll>
-        ):  <Spinner />}
+        <div className='h-[calc(100vh-56px)] w-full'>
+          {videos.length ? (
+            <InfiniteScroll
+              dataLength={videos.length}
+              next={() => dispatch(getHomePageVideos(true))}
+              hasMore={videos.length < 500}
+              loader={<Spinner />}
+              height={800}
+            >
+              <div className='grid gap-y-14 gap-x-8 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-5 p-4 w-full'> {/* sm:grid-cols-1 */}
+                {videos.map((item: HomePageVideos) => {
+                  return <Card data={item} key={item.videoId} />;
+                })}
+              </div>
+            </InfiniteScroll>
+          ):  <Spinner />}
+        </div>
       </div>
     </div>
   )
